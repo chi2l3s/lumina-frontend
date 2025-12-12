29100df3543f3b3a53584da5904f77d6c792a905
chi2l3s <amixolplex@gmail.com>
2025-12-12 19:36:13 +0700

    update anti vun

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
-import { ur } from "zod/v4/locales";
 
 export default function middleware(req: NextRequest) {
-    const { url, cookies, nextUrl } = req
-    
-    const session = cookies.get('session')?.value
+    const { url, cookies, nextUrl } = req;
 
-    const isAuthRoute = nextUrl.pathname.startsWith('/auth')
-    const isAdminRoute = nextUrl.pathname.startsWith('/dashboard')
+    const session = cookies.get("session")?.value;
+
+    const isAuthRoute = nextUrl.pathname.startsWith("/auth");
+    const isAdminRoute = nextUrl.pathname.startsWith("/dashboard");
 
     if (!session && !isAuthRoute) {
-        return NextResponse.redirect(new URL('/auth/login', url))
+        return NextResponse.redirect(new URL("/auth/login", url));
     }
 
     // if (isAdminRoute) {
     //     return NextResponse.redirect(new URL('/', url))
     // }
 
-    return NextResponse.next()
+    return NextResponse.next();
 }
+
+export const config = {
+    matcher: [
+        "/((?!api|_next/static|_next/image|_next/data|favicon.ico|robots.txt|sitemap.xml).*)",
+    ],
+};
