5920daa1165003ded1014569d210b82809b4fbd2
chi2l3s <amixolplex@gmail.com>
2025-12-12 18:31:52 +0700

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,9 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@next/next/no-img-element": "off",
-      "react-hooks/exhaustive-deps": "off"
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
     },
   },
 ];
